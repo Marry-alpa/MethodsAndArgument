d679e4c89c6e7a40a738adbef422708cba1d8680
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2023-10-25 15:43:13 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
+import static java.lang.StrictMath.sqrt;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        welcome();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your first number: ");
+        int a = s.nextInt();
+        System.out.println("Enter your second number: ");
+        int b = s.nextInt();
+        sum(a, b);
+        string();
+
+    }
+    public static void sum(int a, int b){
+        int sum = a + b;
+        System.out.println(sum);
+    }
+    public static void welcome(){
+        String str = "Welcome to system";
+        int i;
+        for(i=1; i<2; i++){
+            System.out.println(str);
+        }
+    }
+    public static String string(){
+        String str = "Buy";
+        System.out.println(str);
+        return str;
     }
 }
